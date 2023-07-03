ef000d47bb5ba7e0189f7475bba3ecead9abaf61
ideepakpgx <138460826+ideepakpgx@users.noreply.github.com>
2023-07-03 15:23:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿function buttonClick(val) {
+    document.getElementById("output").value += val;
+}
+function clearOutput() {
+    document.getElementById("output").value = "";
+}
+function equalClick() {
+    var text = document.getElementById("output").value;
+    var result = eval(text);
+    document.getElementById("output").value = result;
+}
+function calculatePercentage() {
+    var output = document.getElementById("output");
+    var currentValue = parseFloat(output.value);
+    var percentageValue = currentValue / 100;
+    output.value = percentageValue;
+}
+function deleteNumber() {
+    var result = document.getElementById("output");
+    var currentNumber = output.value;
+    var newNumber = currentNumber.slice(0, -1);
+    result.value = newNumber;
+}
+function doubleZero() {
+    var result = document.getElementById("output").value += "00";
+}
+
